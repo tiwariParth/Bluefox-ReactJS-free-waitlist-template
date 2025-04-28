e092deae5453dfacab96a0cd97103f2011f40ae0
tiwariParth <243mjparth@gmail.com>
2025-04-28 08:09:51 +0000

    fix: update handleSubmit to use process.env for sublist URL and remove unused auth token

--- src/App.jsx
+++ src/App.jsx
@@ -84,14 +84,14 @@ export default function App() {
     setLoading(true);
 
     try {
-      const sublistURL = import.meta.env.VITE_BLUEFOX_SUBLIST_URL;
+      // const sublistURL = import.meta.env.VITE_BLUEFOX_SUBLIST_URL;
       // const authToken = import.meta.env.VITE_BLUEFOX_AUTH;
 
       // if (!sublistURL || !authToken) {
       //   throw new Error('Missing API credentials. Please check your .env file.');
       // }
 
-      const response = await fetch(`${sublistURL}`, {
+      const response = await fetch(`${process.env.VITE_BLUEFOX_SUBLIST_URL}`, {
         method: 'POST',
         headers:{
          'Content-Type': 'application/json',
